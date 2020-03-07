d506ef960a2688e8e2212414614bdf78d8bc1063
Rohan Bojja <rohanbojja@outlook.com>
2020-03-08 05:06:08 +0530

    Fixed broken packages and clean up.
    Dropped unnecessary files.
    Updated requirements.txt
    
    Changes:
    librosa.feature.rmse is now librosa.feature.rms.

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class receiveWav(Resource):
         spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
         rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
         zcr = librosa.feature.zero_crossing_rate(y)
-        rmse = librosa.feature.rmse(y=y)
+        rmse = librosa.feature.rms(y=y)
         mfcc = librosa.feature.mfcc(y=y, sr=sr)
         features = f'{np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
         for e in mfcc:
